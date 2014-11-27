175d3fba49a340587bf8211365c1cf5d1e364a54
0xack13 <0xack13@gmail.com>
2014-11-27 09:38:33 +0300

    Initial commit

---
+++ CustomReflection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine("Name of the assembly: " + assembly.FullName);
+        }
+    }
+}
